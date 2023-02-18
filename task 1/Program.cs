ba75f9c814335f7b76ea3b206838242fdf35293c
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-18 10:50:30 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine($"{msg}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if(num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("This number not three-digit mate.");
+    return false;
+}
+
+int number = Prompt("Input three-digit number");
+if(Validate(number))
+{
+    number = number /10 %10;
+    System.Console.WriteLine($"Second digit is the {number}");
+}
